a6772cc5458e0281261cb6655391c087f9b23be0
brendanjamesmulhern <brendanjamesmulhern@gmail.com>
2022-04-01 13:31:23 -0400

    deploying to the cloud

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            List<string> strList = new List<string>();
+            strList.Add("Test");
+            strList.Add("Test2");
+            strList.Add("Test3");
+            strList.Add("Test5");
+            strList.Add("Test6");
+            strList.Add("Test7");
+            strList.Add("Test8");
+            strList.Add("Test9");
+            strList.Add("Test11");
+
+            foreach(var str in strList)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
